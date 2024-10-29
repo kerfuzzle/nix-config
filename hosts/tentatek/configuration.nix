985df8569644d366c3d69211bf60bbffcf2dd203
kerfuzzle <58907164+kerfuzzle@users.noreply.github.com>
2024-10-29 23:16:36 +0000

    Added openRGB

--- hosts/tentatek/configuration.nix
+++ hosts/tentatek/configuration.nix
@@ -9,6 +9,7 @@
     [
       ./hardware-configuration.nix
       ./graphics.nix
+			./rgb.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
---
+++ hosts/tentatek/rgb.nix
@@ -0,0 +1,28 @@
+{ pkgs, ... }: let
+
+	openrgb-pipeline = pkgs.openrgb-with-all-plugins.overrideAttrs {
+		version = "pipeline";
+		src = pkgs.fetchFromGitLab {
+			owner = "CalcProgrammer1";
+			repo = "OpenRGB";
+			rev = "8565a3ddee581f319f63a7b506111fbb6e4d9f0c";
+			sha256 = "sha256-pO6sp3mA8x2cQmB62SWUaAaQvyJOUG5KTMHLAYDG8ck=";
+		};
+
+		postPatch = ''
+   		patchShebangs scripts/build-udev-rules.sh
+    	substituteInPlace scripts/build-udev-rules.sh \
+      	--replace /usr/bin/env "${pkgs.coreutils}/bin/chmod"
+		'';
+	};
+in {
+	environment.systemPackages = [
+		openrgb-pipeline
+	];
+
+	services.hardware.openrgb = {
+		enable = true;
+		motherboard = "amd";
+		package = openrgb-pipeline;
+	};
+}
