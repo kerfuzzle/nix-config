ae2265e5928ba79a23395043d67f77dd725e8c28
kerfuzzle <58907164+kerfuzzle@users.noreply.github.com>
2024-12-03 08:52:28 +0000

    Updated window rules

--- modules/home-manager/wm/hyprland.nix
+++ modules/home-manager/wm/hyprland.nix
@@ -57,6 +57,7 @@ in {
 
 			windowrulev2 = [
 				"opacity 1.0 1.0 override, title:(YouTube)"
+				"opacity 1.0 1.0 override, title:(Picture-in-Picture)"
 				"opacity 1.0 1.0 override, class:^(discord)"
 				"opacity 1.0 1.0 override, class:^(Code)"
 			];
