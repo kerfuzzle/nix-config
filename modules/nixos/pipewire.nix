688d8ba2af5671d348785a3ddd99f10d6f87037b
kerfuzzle <58907164+kerfuzzle@users.noreply.github.com>
2025-02-18 15:31:03 +0000

    Added icon override for dawn pro dac

--- modules/nixos/pipewire.nix
+++ modules/nixos/pipewire.nix
@@ -24,6 +24,23 @@ in {
 						"default.clock.allowed-rates" = [ 44100 48000 64000 88200 96000 128000 176400 192000 256000 352800 384000 512000 705600 768000 ];
 					};
 				};
+
+				"10-dawn-pro" = {
+					"device.rules" = [
+						{
+							"matches" = [
+								{
+									"device.product.name" = "MOONDROP Dawn Pro";
+								}
+							];
+							"actions" = {
+								"update-props" = {
+									"device.form-factor" = "headphone";
+								};
+							};
+						}
+					];
+				};
 			};
 
 			client = resample-quality;
