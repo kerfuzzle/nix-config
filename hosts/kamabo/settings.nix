d55ba568c757e1c57c75472cf5cca64dc27a3156
kerfuzzle <58907164+kerfuzzle@users.noreply.github.com>
2025-02-19 21:05:58 +0000

    changed wall on kamambo

--- hosts/kamabo/settings.nix
+++ hosts/kamabo/settings.nix
@@ -2,6 +2,6 @@
   hostname = "kamabo";
   username = "kerfuzzle";
   colorScheme = "catppuccin-frappe";
-  wallpaper = ../../assets/walls/flowers.jpg;
+  wallpaper = ../../assets/walls/nix-frappe.png;
   monitors = [ "eDP-1,1920x1080@144,0x0,1" "HDMI-A-2,1920x1080@60,1920x0,1" ];
 }
