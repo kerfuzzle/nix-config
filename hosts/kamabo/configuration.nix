995a2ea1f3db43b9193f725111a493c3554dab31
kerfuzzle <58907164+kerfuzzle@users.noreply.github.com>
2025-02-26 22:05:59 +0000

    Modified kamabo config for improved battery life

--- hosts/kamabo/configuration.nix
+++ hosts/kamabo/configuration.nix
@@ -60,28 +60,28 @@
   };
 
 	#services.power-profiles-daemon.enable = true;
-	#services.tlp = {
-	#	enable = true;
-	#	settings = {
-	#		CPU_SCALING_GOVERNOR_ON_AC = "performance";
-	#		CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-	#		CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-	#		CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-	#	};
-	#};
-	services.auto-cpufreq  = {
+	services.tlp = {
 		enable = true;
 		settings = {
-			battery = {
-				governor = "powersave";
-				turbo = "never";
-			};
-			charger = {
-				governor = "performance";
-				turbo = "auto";
-			};
+			CPU_SCALING_GOVERNOR_ON_AC = "performance";
+			CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+			CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+			CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 		};
 	};
+	#services.auto-cpufreq  = {
+	#	enable = true;
+	#	settings = {
+	#		battery = {
+	#			governor = "powersave";
+	#			turbo = "never";
+	#		};
+	#		charger = {
+	#			governor = "performance";
+	#			turbo = "auto";
+	#		};
+	#	};
+	#};
   # Enable touchpad support (enabled default in most desktopManager).
   # services.libinput.enable = true;
 
