af9a0787c2927d3deeb3d27b34b0467c05fc2471
kerfuzzle <58907164+kerfuzzle@users.noreply.github.com>
2025-07-23 11:28:44 +0100

    Firefox: Removed privacy badger and fixed the default_area for extensions

--- modules/home-manager/apps/firefox/extensions.nix
+++ modules/home-manager/apps/firefox/extensions.nix
@@ -1,33 +1,26 @@
 let
-  mkExtension = shortId: uuid: {
+  mkExtension = shortId: uuid: pinned: {
     name = uuid;
     value = {
       install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
       installation_mode = "force_installed";
-      default_area = "menupanel";
+      default_area = if pinned then "navbar" else "menupanel";
     };
   };
 in
 {
   programs.firefox.policies.ExtensionSettings = builtins.listToAttrs [
     # Adblock
-    (mkExtension "ublock-origin" "uBlock0@raymondhill.net")
+    (mkExtension "ublock-origin" "uBlock0@raymondhill.net" false)
     # Skips youtube sponsor segements
-    (mkExtension "sponsorblock" "sponsorBlocker@ajay.app")
+    (mkExtension "sponsorblock" "sponsorBlocker@ajay.app" false)
     # Tab groups
-    (mkExtension "simple-tab-groups" "simple-tab-groups@drive4ik")
+    (mkExtension "simple-tab-groups" "simple-tab-groups@drive4ik" true)
     # Containers to seperate different accounts
-    (
-      (mkExtension "multi-account-containers" "@testpilot-containers")
-      // {
-        value.default_area = "navbar";
-      }
-    )
-    # Tracker blocking
-    (mkExtension "privacy-badger17" "jid1-MnnxcxisBPnSXQ@jetpack")
+    (mkExtension "multi-account-containers" "@testpilot-containers" false)
     # Translates web pages
-    (mkExtension "traduzir-paginas-web" "{036a55b4-5e72-4d05-a06c-cba2dfcc134a}")
+    (mkExtension "traduzir-paginas-web" "{036a55b4-5e72-4d05-a06c-cba2dfcc134a}" false)
     # Strips tracking elements from URLs
-    (mkExtension "clearurls" "{74145f27-f039-47ce-a470-a662b129930a}")
+    (mkExtension "clearurls" "{74145f27-f039-47ce-a470-a662b129930a}" false)
   ];
 }
