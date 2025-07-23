9d18f61ccea0e3677a77eece9c0d5843b48c2cc2
kerfuzzle <58907164+kerfuzzle@users.noreply.github.com>
2025-07-23 15:22:12 +0100

    Reorganised nix config and added a GH access token to avoid rate limits

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,4 +1,10 @@
 {
+  outputs,
+  config,
+  lib,
+  ...
+}:
+{
   imports = [
     ./network
     ./users
@@ -12,7 +18,6 @@
     ./greetd.nix
     ./kernel.nix
     ./cachix.nix
-    ./store.nix
     ./droidcam.nix
     ./tablet.nix
     ./zsh.nix
@@ -25,9 +30,44 @@
     ./hyprland.nix
   ];
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-    "pipe-operators"
-  ];
+  sops = {
+    secrets.github-access-token.sopsFile = lib.custom.configRoot + /secrets/misc.yaml;
+    templates.access-token-prelude = {
+      # Access token only has access to read public repositories. This avoids github rate limiting issues.
+      content = ''
+        				access-tokens = github.com=${config.sops.placeholder.github-access-token}
+        			'';
+      # Must be accessible to user as the nix evaluator does not run as root
+      owner = config.hostConfig.username;
+      mode = "0400";
+    };
+  };
+
+  nixpkgs.overlays = outputs.overlays.all;
+
+  nix = {
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+        "pipe-operators"
+      ];
+
+      # Automatically optimise store on rebuild
+      auto-optimise-store = true;
+    };
+
+    # Automatically run garbage collection every day
+    gc = {
+      automatic = true;
+      dates = "daily";
+      # Delete generations more than 5 days old
+      options = "--delete-older-than 5d";
+    };
+
+    # Inlclude the token config in nix.conf
+    extraOptions = ''
+      			!include ${config.sops.templates.access-token-prelude.path}
+      		'';
+  };
 }
--- modules/nixos/store.nix
+++
@@ -1,9 +0,0 @@
-{
-  nix.settings.auto-optimise-store = true;
-
-  nix.gc = {
-    automatic = true;
-    dates = "daily";
-    options = "--delete-older-than 5d";
-  };
-}
