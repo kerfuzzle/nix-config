962a680b4e1f4c2ed0bb6be5454e7d9c37fca210
kerfuzzle <58907164+kerfuzzle@users.noreply.github.com>
2025-08-20 11:12:47 +0100

    Added thunderbird email client

---
+++ modules/home-manager/apps/email.nix
@@ -0,0 +1,30 @@
+{
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.homeConfig.email;
+in
+{
+  options.homeConfig.email.enable = lib.mkEnableOption "email client" // {
+    default = true;
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.thunderbird = {
+      enable = true;
+      profiles.${config.home.username} = {
+        isDefault = true;
+      };
+      settings = {
+        # Disable in app updates, managed by nix
+        "app.update.enabled" = false;
+        # Tracking prevention
+        "privacy.globalprivacycontrol.enabled" = true;
+        # Disable checking if thunderbird is default mail client
+        "mail.shell.checkDefaultClient" = false;
+      };
+    };
+  };
+}
