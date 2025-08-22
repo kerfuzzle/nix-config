17445d555a879a3315f97711ee453e76d3a5f094
kerfuzzle <58907164+kerfuzzle@users.noreply.github.com>
2025-08-22 10:32:50 +0100

    firefox: Enabled autoscrolling

--- modules/home-manager/apps/firefox/default.nix
+++ modules/home-manager/apps/firefox/default.nix
@@ -16,6 +16,8 @@
         "browser.startup.page" = 3;
         # Disable bookmarks bar
         "browser.toolbars.bookmarks.visibility" = "never";
+        # Enable autoscrolling
+        "general.autoScroll" = true;
       };
 
       search = {
