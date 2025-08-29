d6225bf6bbe1ff255ba596062aec4df584527a6a
kerfuzzle <58907164+kerfuzzle@users.noreply.github.com>
2025-08-29 13:49:06 +0100

    Added option to configure how fn keys on apple keyboards behave

--- modules/nixos/kernel.nix
+++ modules/nixos/kernel.nix
@@ -4,13 +4,31 @@
   lib,
   ...
 }:
+let
+  cfg = config.hostConfig.kernel;
+in
 {
-  options.hostConfig.useLatestKernel = lib.mkOption {
-    description = "Whether the system should use the latest kernel release, else use the latest LTS release";
-    default = true;
-    type = lib.types.bool;
+  options.hostConfig.kernel = {
+    useLatest = lib.mkOption {
+      description = "Whether the system should use the latest kernel release, else use the latest LTS release";
+      default = true;
+      type = lib.types.bool;
+    };
+    hidAppleFnMode = lib.mkOption {
+      description = ''
+        				Modifies how function keys act on Apple Keyboards
+        				0 - disabled
+        				1 - normally media keys, switchable to function keys by holding Fn key (=auto on Apple keyboards)
+        				2 - normally function keys, switchable to media keys by holding Fn key (=auto on non-Apple keyboards)
+        				3 - auto
+        			'';
+      default = 2;
+      type = lib.types.ints.between 0 3;
+    };
   };
 
-  config.boot.kernelPackages =
-    if config.hostConfig.useLatestKernel then pkgs.linuxPackages_latest else pkgs.linuxPackages;
+  config.boot = {
+    kernelPackages = lib.mkIf cfg.useLatest pkgs.linuxPackages_latest;
+    extraModprobeConfig = "options hid_apple fnmode=${toString cfg.hidAppleFnMode}";
+  };
 }
