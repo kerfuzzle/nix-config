929e6a18c996bf2f848a0a48f6f7d66df86b3706
kerfuzzle <58907164+kerfuzzle@users.noreply.github.com>
2025-09-06 09:02:56 +0100

    vscode: added homeConfig option to toggle vscode

--- modules/home-manager/apps/vscode.nix
+++ modules/home-manager/apps/vscode.nix
@@ -1,11 +1,16 @@
-{ pkgs, ... }:
-{
-  stylix.targets.vscode.enable = false;
-  home.packages = with pkgs; [
-    nixd
-  ];
-  allowedUnfreePkgs = [
-    "vscode"
-  ];
-  programs.vscode.enable = true;
+{ pkgs, config, lib, ... }: let
+  cfg = config.homeConfig.vscode;
+in {
+  options.homeConfig.vscode.enable = lib.mkEnableOption "vscode";
+
+  config = lib.mkIf cfg.enable {
+    stylix.targets.vscode.enable = false;
+    home.packages = with pkgs; [
+      nixd
+    ];
+    allowedUnfreePkgs = [
+      "vscode"
+    ];
+    programs.vscode.enable = true;
+  };
 }
