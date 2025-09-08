96b7be4da03b04455d8a595af3e72af9a4b1356a
kerfuzzle <58907164+kerfuzzle@users.noreply.github.com>
2025-09-08 21:51:16 +0100

    zathura: added config options to HM

--- modules/home-manager/apps/zathura.nix
+++ modules/home-manager/apps/zathura.nix
@@ -1,5 +1,11 @@
 {
   programs.zathura = {
     enable = true;
+    options = {
+      # Open document zoomed to page width
+      adjust-open = "width";
+      # Make shift-click trigger SyncTeX
+      synctex-edit-modifier = "shift";
+    };
   };
 }
