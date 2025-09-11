c50251dec5cca0f86837a6adbdb67199c0fd92e6
kerfuzzle <58907164+kerfuzzle@users.noreply.github.com>
2025-09-11 22:29:57 +0100

    zathura: overwritten stylix color for dark scheme text

--- modules/home-manager/apps/zathura.nix
+++ modules/home-manager/apps/zathura.nix
@@ -1,11 +1,20 @@
+{ config, lib, ... }:
 {
   programs.zathura = {
     enable = true;
-    options = {
-      # Open document zoomed to page width
-      adjust-open = "width";
-      # Make shift-click trigger SyncTeX
-      synctex-edit-modifier = "shift";
-    };
+    options =
+      let
+        inherit (config.lib.stylix) colors;
+        getColorCh = colorName: channel: colors."${colorName}-rgb-${channel}";
+        rgb = color: ''rgb(${getColorCh color "r"}, ${getColorCh color "g"}, ${getColorCh color "b"})'';
+      in
+      {
+        # Open document zoomed to page width
+        adjust-open = "width";
+        # Make shift-click trigger SyncTeX
+        synctex-edit-modifier = "shift";
+
+        recolor-darkcolor = lib.mkForce (rgb "base05");
+      };
   };
 }
