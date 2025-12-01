9c98727ae45f811b79d96381bc9446940a10af80
kerfuzzle <58907164+kerfuzzle@users.noreply.github.com>
2025-12-01 08:48:07 +0000

    Flake update

--- modules/home-manager/terminal/alacritty.nix
+++ modules/home-manager/terminal/alacritty.nix
@@ -6,7 +6,7 @@
 {
   programs.alacritty = {
     enable = true;
-    package = pkgs.alacritty-graphics;
+    package = pkgs.alacritty;
     settings = {
       window = {
         # Override stylix default
