8a49e13968e206a1c42f66a499788b85f5b0a0d9
kerfuzzle <58907164+kerfuzzle@users.noreply.github.com>
2026-01-01 11:18:49 +0000

    overlays: override statix to lastest commit

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,14 +4,31 @@ rec {
     custom = import (lib.custom.configRoot + /pkgs) final.pkgs;
   };
 
+  updates = _final: prev: {
+    statix = prev.statix.overrideAttrs (_old: rec {
+      src = prev.fetchFromGitHub {
+        owner = "oppiliappan";
+        repo = "statix";
+        rev = "e9df54ce918457f151d2e71993edeca1a7af0132";
+        hash = "sha256-duH6Il124g+CdYX+HCqOGnpJxyxOCgWYcrcK0CBnA2M=";
+      };
+
+      cargoDeps = prev.pkgs.rustPlatform.importCargoLock {
+        lockFile = src + "/Cargo.lock";
+        allowBuiltinFetchGit = true;
+      };
+    });
+  };
+
   stable-packages = final: _prev: {
     stable = import inputs.nixpks-stable {
-      system = final.system;
+      inherit (final) system;
     };
   };
 
   all = [
     additions
+    updates
     stable-packages
   ];
 }
