44e1fff54a9f80c9b2719517864ec902b70b5e2b
kerfuzzle <58907164+kerfuzzle@users.noreply.github.com>
2026-05-25 12:51:30 +0100

    Disable custom systemd nvidia power management services

--- modules/nixos/graphics/nvidia.nix
+++ modules/nixos/graphics/nvidia.nix
@@ -54,35 +54,35 @@ in
     };
 
     # Fixes suspend-then-hibernate
-    systemd.services = lib.mkIf config.hardware.nvidia.powerManagement.enable (
-      let
-        mkNvidiaSuspendService = state: sleepAction: {
-          description = "NVIDIA system ${state} actions";
-          path = [ pkgs.kbd ];
-          serviceConfig = {
-            Type = "oneshot";
-            ExecStart = "${config.hardware.nvidia.package.out}/bin/nvidia-sleep.sh '${sleepAction}'";
-          };
-          before = [ "systemd-${state}.service" ];
-          requiredBy = [ "systemd-${state}.service" ];
-        };
-      in
-      {
-        nvidia-suspend-then-hibernate = mkNvidiaSuspendService "suspend-then-hibernate" "suspend";
-        # nvidia-hybrid-sleep = mkNvidiaSuspendService "hybrid-sleep" "hibernate";
+    # systemd.services = lib.mkIf config.hardware.nvidia.powerManagement.enable (
+    #   let
+    #     mkNvidiaSuspendService = state: sleepAction: {
+    #       description = "NVIDIA system ${state} actions";
+    #       path = [ pkgs.kbd ];
+    #       serviceConfig = {
+    #         Type = "oneshot";
+    #         ExecStart = "${config.hardware.nvidia.package.out}/bin/nvidia-sleep.sh '${sleepAction}'";
+    #       };
+    #       before = [ "systemd-${state}.service" ];
+    #       requiredBy = [ "systemd-${state}.service" ];
+    #     };
+    #   in
+    #   {
+    #     nvidia-suspend-then-hibernate = mkNvidiaSuspendService "suspend-then-hibernate" "suspend";
+    #     # nvidia-hybrid-sleep = mkNvidiaSuspendService "hybrid-sleep" "hibernate";
 
-        # Add after services to nvidia-resume service
-        nvidia-resume = {
-          after = [
-            "systemd-suspend-then-hibernate.service"
-            # "systemd-hybrid-sleep.service"
-          ];
-          requiredBy = [
-            "systemd-suspend-then-hibernate.service"
-            # "systemd-hybrid-sleep.service"
-          ];
-        };
-      }
-    );
+    #     # Add after services to nvidia-resume service
+    #     nvidia-resume = {
+    #       after = [
+    #         "systemd-suspend-then-hibernate.service"
+    #         # "systemd-hybrid-sleep.service"
+    #       ];
+    #       requiredBy = [
+    #         "systemd-suspend-then-hibernate.service"
+    #         # "systemd-hybrid-sleep.service"
+    #       ];
+    #     };
+    #   }
+    # );
   };
 }
