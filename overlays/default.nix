933cefcd805ec035589d11ef697f842c3cc5aaeb
kerfuzzle <58907164+kerfuzzle@users.noreply.github.com>
2026-09-05 10:55:00 +0100

    Removed statix overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,19 +5,19 @@ rec {
   };
 
   updates = _final: prev: {
-    statix = prev.statix.overrideAttrs (_old: rec {
-      src = prev.fetchFromGitHub {
-        owner = "oppiliappan";
-        repo = "statix";
-        rev = "e9df54ce918457f151d2e71993edeca1a7af0132";
-        hash = "sha256-duH6Il124g+CdYX+HCqOGnpJxyxOCgWYcrcK0CBnA2M=";
-      };
+    # statix = prev.statix.overrideAttrs (_old: rec {
+    #   src = prev.fetchFromGitHub {
+    #     owner = "oppiliappan";
+    #     repo = "statix";
+    #     rev = "e9df54ce918457f151d2e71993edeca1a7af0132";
+    #     hash = "sha256-duH6Il124g+CdYX+HCqOGnpJxyxOCgWYcrcK0CBnA2M=";
+    #   };
 
-      cargoDeps = prev.pkgs.rustPlatform.importCargoLock {
-        lockFile = src + "/Cargo.lock";
-        allowBuiltinFetchGit = true;
-      };
-    });
+    #   cargoDeps = prev.pkgs.rustPlatform.importCargoLock {
+    #     lockFile = src + "/Cargo.lock";
+    #     allowBuiltinFetchGit = true;
+    #   };
+    # });
   };
 
   stable-packages = final: _prev: {
