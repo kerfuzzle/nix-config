d795939bc5d8a4864aa34ad19d23d3ba9d3c927d
kerfuzzle <58907164+kerfuzzle@users.noreply.github.com>
2026-09-12 09:29:31 +0100

    nvidia: update power management

--- modules/nixos/graphics/nvidia.nix
+++ modules/nixos/graphics/nvidia.nix
@@ -39,8 +39,7 @@ in
 
     hardware.nvidia = {
       modesetting.enable = true;
-      # Systemd power management
-      powerManagement.enable = true;
+      powerManagement.finegrained = true;
       # Open source drivers are now recommened
       open = true;
       # Use beta/stable branch
@@ -52,37 +51,5 @@ in
       # GUI gpu info tool
       nvidiaSettings = true;
     };
-
-    # Fixes suspend-then-hibernate
-    # systemd.services = lib.mkIf config.hardware.nvidia.powerManagement.enable (
-    #   let
-    #     mkNvidiaSuspendService = state: sleepAction: {
-    #       description = "NVIDIA system ${state} actions";
-    #       path = [ pkgs.kbd ];
-    #       serviceConfig = {
-    #         Type = "oneshot";
-    #         ExecStart = "${config.hardware.nvidia.package.out}/bin/nvidia-sleep.sh '${sleepAction}'";
-    #       };
-    #       before = [ "systemd-${state}.service" ];
-    #       requiredBy = [ "systemd-${state}.service" ];
-    #     };
-    #   in
-    #   {
-    #     nvidia-suspend-then-hibernate = mkNvidiaSuspendService "suspend-then-hibernate" "suspend";
-    #     # nvidia-hybrid-sleep = mkNvidiaSuspendService "hybrid-sleep" "hibernate";
-
-    #     # Add after services to nvidia-resume service
-    #     nvidia-resume = {
-    #       after = [
-    #         "systemd-suspend-then-hibernate.service"
-    #         # "systemd-hybrid-sleep.service"
-    #       ];
-    #       requiredBy = [
-    #         "systemd-suspend-then-hibernate.service"
-    #         # "systemd-hybrid-sleep.service"
-    #       ];
-    #     };
-    #   }
-    # );
   };
 }
